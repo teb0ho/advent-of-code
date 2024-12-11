ee4b5514bf403e79cb791382e82f56053c88a048
Teboho Mphure <tebohomphure@gmail.com>
2024-12-11 20:52:15 +0200

    add day 3.1

--- 2024/day3.1.dart
+++ 2024/day3.1.dart
@@ -1,3 +1,16 @@
+import 'util.dart';
+
 void main() {
-  var fileContent = readFile('./files/day2.txt');
+  var fileContent = readFile('./files/day3.txt');
+
+  RegExp regex = RegExp(r'mul\((\d+),(\d+)\)');
+
+  var matches = regex.allMatches(fileContent);
+  var total = 0;
+
+  for (final match in matches) {
+    total += int.parse(match.group(1) ?? '') * int.parse(match.group(2) ?? '');
+  }
+
+  print(total);
 }
