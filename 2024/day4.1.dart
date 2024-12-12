5a58939fe20a9ff31ffdf0009a6645ec2e91fad8
Teboho Mphure <tebohomphure@gmail.com>
2024-12-12 21:31:27 +0200

    add day4.1

---
+++ 2024/day4.1.dart
@@ -0,0 +1,17 @@
+import 'dart:collection';
+
+import 'util.dart';
+
+void main() {
+  var fileContent = readFile('./files/day4.txt');
+
+  var lines = fileContent.split('\n');
+
+  final HashMap<int, String> words = HashMap();
+
+  var count = 0;
+  var xmas = 'XMAS';
+  var samx = 'SAMX';
+
+  for (final line in lines) {}
+}
