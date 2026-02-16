382a0d3d006d7d627980cbca22a7ac28f03d1a78
Teboho <tebohomphure@gmail.com>
2026-02-16 14:42:17 +0200

    add day2 v2

---
+++ 2025/day5.2.v2.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func cafeteria2v2() {
+	file, err := os.Open("./files/day5.txt")
+
+	if err != nil {
+		fmt.Printf("Error opening file: %v\n", err)
+		return
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	results := []int{}
+	list := []string{}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if strings.TrimSpace(line) == "" {
+			break
+		}
+
+		list = append(list, line)
+	}
+
+	var finalList [][2]int
+	for _, val := range list {
+
+	}
+}
