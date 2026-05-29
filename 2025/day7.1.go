87706bc5bad597771e7d71aeb42cc8cec014ff44
Teboho <tebohomphure@gmail.com>
2026-05-29 23:56:28 +0200

    add regex

--- 2025/day7.1.go
+++ 2025/day7.1.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 )
 
@@ -11,6 +12,13 @@ func laboratoriesPart1(input []string) {
 	for i := 1; i < len(input); i++ {
 		if position != -1 && !strings.Contains(input[i], "^") {
 			input[i] = input[i][:position] + "|" + input[i][position+1:]
+		} else {
+			re := regexp.MustCompile(`\^`)
+			indices := re.FindAllStringIndex(input[i], -1)
+
+			for _, index := range indices {
+
+			}
 		}
 	}
 
