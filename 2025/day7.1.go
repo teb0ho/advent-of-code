77dff721996d18d6638cf7e4d7e3c72c4d89c330
Teboho <tebohomphure@gmail.com>
2026-06-05 21:55:47 +0200

    wip 1

--- 2025/day7.1.go
+++ 2025/day7.1.go
@@ -8,19 +8,36 @@ import (
 
 func laboratoriesPart1(input []string) {
 	position := strings.Index(input[0], "S")
-	nextIndeices := []int{}
+	nextIndices := []int{}
 
 	for i := 1; i < len(input); i++ {
-		if position != -1 && !strings.Contains(input[i], "^") {
+		if nextIndices != nil && len(nextIndices) > 0 {
+			var modifiedString string
+
+			for _, nextIndex := range nextIndices {
+				modifiedString = input[i]
+				modifiedString = modifiedString[:nextIndex] + "|" + modifiedString[nextIndex+1:]
+				input[i] = modifiedString
+			}
+
+			nextIndices = []int{}
+		}
+
+		if i == 1 && position != -1 && !strings.Contains(input[i], "^") {
 			input[i] = input[i][:position] + "|" + input[i][position+1:]
 		} else if strings.Contains(input[i], "|") {
+			re := regexp.MustCompile(`\|`)
+			indices := re.FindAllStringIndex(input[i], -1)
+			for _, index := range indices {
+				nextIndices = append(nextIndices, index[0])
+			}
 		} else {
 			re := regexp.MustCompile(`\^`)
 			indices := re.FindAllStringIndex(input[i], -1)
 
 			for _, index := range indices {
-              if len(indices) > 0 {
-			  }
+				if len(indices) > 0 {
+				}
 			}
 		}
 	}
