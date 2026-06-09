c62552abecb6601bd4d811dfbfd1506a5d2d95cf
Teboho <tebohomphure@gmail.com>
2026-06-09 21:56:03 +0200

    Refactor laboratoriesPart1 function to simplify logic and remove unused code

--- 2025/day7.1.go
+++ 2025/day7.1.go
@@ -2,44 +2,15 @@ package main
 
 import (
 	"fmt"
-	"regexp"
 	"strings"
 )
 
 func laboratoriesPart1(input []string) {
-	position := strings.Index(input[0], "S")
-	nextIndices := []int{}
+	sPosition := strings.Index(input[0], "S")
 
 	for i := 1; i < len(input); i++ {
-		if nextIndices != nil && len(nextIndices) > 0 {
-			var modifiedString string
-
-			for _, nextIndex := range nextIndices {
-				modifiedString = input[i]
-
-				if input[i][nextIndex] != '^' {
-				}
-				modifiedString = modifiedString[:nextIndex] + "|" + modifiedString[nextIndex+1:]
-				input[i] = modifiedString
-			}
-
-			nextIndices = []int{}
-		}
-
-		if i == 1 && position != -1 && !strings.Contains(input[i], "^") {
-			input[i] = input[i][:position] + "|" + input[i][position+1:]
-		} else if strings.Contains(input[i], "|") {
-			re := regexp.MustCompile(`\|`)
-			indices := re.FindAllStringIndex(input[i], -1)
-
-			for _, index := range indices {
-				nextIndices = append(nextIndices, index[0])
-			}
-
-		} else {
-			re := regexp.MustCompile(`\^`)
-			indices := re.FindAllStringIndex(input[i], -1)
-
+		if string(input[i][sPosition]) != "^" {
+			input[i] = input[i][:sPosition] + "|" + input[i][sPosition+1:]
 		}
 	}
 
@@ -47,6 +18,4 @@ func laboratoriesPart1(input []string) {
 
 }
 
-func seekPipes(input string) {
-	
-}
+func seekPipes(input string) {}
