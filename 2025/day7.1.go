8342085345c0503e748f01b2d5b4704972c22b77
Teboho <tebohomphure@gmail.com>
2026-06-09 22:05:00 +0200

    Update launch configuration and enhance laboratoriesPart1 logic for pipe handling

--- 2025/day7.1.go
+++ 2025/day7.1.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 )
 
@@ -9,8 +10,14 @@ func laboratoriesPart1(input []string) {
 	sPosition := strings.Index(input[0], "S")
 
 	for i := 1; i < len(input); i++ {
-		if string(input[i][sPosition]) != "^" {
+		if i == 1 && string(input[i][sPosition]) != "^" {
 			input[i] = input[i][:sPosition] + "|" + input[i][sPosition+1:]
+		} else {
+			// check pipes in previous row and add pipes to the current row
+			re := regexp.MustCompile(`\|`)
+			pipePositions := re.FindAllStringIndex(input[i-1], -1)
+			// if index has . insert pipe else if it has a caret ^ add pipes to the left and right of the caret
+			// and count the pipe and caret collisions
 		}
 	}
 
