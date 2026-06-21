f24032d02bb072b6badd247b4f6df853f7e8dfc9
Teboho <tebohomphure@gmail.com>
2026-06-22 00:04:21 +0200

    Refactor laboratoriesPart2 function to implement pipe and caret collision logic

--- 2025/day7.2.go
+++ 2025/day7.2.go
@@ -1,8 +1,32 @@
 package main
 
-import "strings"
+import (
+	"regexp"
+	"strings"
+)
 
 func laboratoriesPart2(input []string) {
 	sPosition := strings.Index(input[0], "S")
-	collisions := 0
+
+	for i := 1; i < len(input); i++ {
+		if i == 1 && string(input[i][sPosition]) != "^" {
+			input[i] = input[i][:sPosition] + "|" + input[i][sPosition+1:]
+		} else {
+			// check pipes in previous row and add pipes to the current row
+			re := regexp.MustCompile(`\|`)
+			pipePositions := re.FindAllStringIndex(input[i-1], -1)
+			// if index has . insert pipe else if it has a caret ^ add pipes to the left and right of the caret
+			// and count the pipe and caret collisions
+			for _, pos := range pipePositions {
+				if string(input[i][pos[0]]) == "." {
+					input[i] = input[i][:pos[0]] + "|" + input[i][pos[0]+1:]
+				} else if string(input[i][pos[0]]) == "^" {
+					out := []rune(input[i])
+					out[pos[0]-1] = '|'
+					out[pos[0]+1] = '|'
+					input[i] = string(out)
+				}
+			}
+		}
+	}
 }
