df67089b08cef4525f8bb9dbc224aaa6268c0869
Teboho <tebohomphure@gmail.com>
2026-07-12 23:45:21 +0200

    Fix loop iteration in laboratoriesPart2 function to process input from start to end

--- 2025/day7.2.go
+++ 2025/day7.2.go
@@ -34,7 +34,7 @@ func laboratoriesPart2(input []string) {
 
 	// use new modified input to search all valid pipe paths
 	fileLength := len(input)
-	for i := len(input) - 1; i >= 1; i-- {
+	for i := 1; i < fileLength; i++ {
 		re := regexp.MustCompile(`\|`)
 		pipePositions := re.FindAllStringIndex(input[i-1], -1)
 
