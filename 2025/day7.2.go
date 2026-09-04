0ef5822afe5aa017a8f3509e5ad0ed012e4ffb7d
Teboho Mphure <tebohomphure@gmail.com>
2026-09-04 23:34:07 +0200

    Refactor path traversal logic in laboratoriesPart2 to enhance tree structure management

--- 2025/day7.2.go
+++ 2025/day7.2.go
@@ -38,49 +38,67 @@ func laboratoriesPart2(input []string) {
 		}
 	}
 
-	var paths []PathIndex = []PathIndex{}
-
-	fileLength := len(input) - 1
 	var tree map[string][]string = make(map[string][]string)
 	// traverse all paths and check if they are valid
 	// LLDDRR
-	previous := []int{}
-	previousKey := ""
-	for i := 1; i < len(input); i++ {
-		if i == 1 {
+	nodes := []int{}
+	nodeKey := ""
+	for row := 1; row < len(input); row++ {
+		if row == 1 {
 			re := regexp.MustCompile(`\|`)
-			pipePositions := re.FindStringIndex(input[i])
-			index := pipePositions[0]
-			tree[fmt.Sprintf("%d,%d", i, index)] = []string{}
-			previous = []int{index}
-			previousKey = fmt.Sprintf("%d,%d", i-1, previous[0])
+			pipePositions := re.FindStringIndex(input[row])
+			column := pipePositions[0]
+			tree[fmt.Sprintf("%d,%d", row, column)] = []string{}
+			nodes = []int{column}
+			nodeKey = fmt.Sprintf("%d,%d", row, nodes[0])
 		} else {
-			if len(previous) != 0 {
-				for _, prev := range previous {
-					if string(input[i][prev]) == "|" {
-						tree[previousKey] = []string{fmt.Sprintf("%d,%d", i, previous[0])}
+			if len(nodes) != 0 {
+				newNodeKeys := []int{}
+				for _, col := range nodes {
+					if string(input[row][col]) == "|" {
+						tree[nodeKey] = []string{fmt.Sprintf("%d,%d", row, col)}
+						nodeKey = fmt.Sprintf("%d,%d", row, col)
+						newNodeKeys = append(newNodeKeys, col)
 
-					} else if string(input[i][prev]) == "^" {
-						tree[previousKey] = []string{fmt.Sprintf("%d,%d", i, prev-1), fmt.Sprintf("%d,%d", i, prev+1)}
-						previous = []int{prev - 1, prev + 1}
-						previousKey = fmt.Sprintf("%d,%d", i-1, prev)
+					} else if string(input[row][col]) == "^" {
+						tree[nodeKey] = []string{fmt.Sprintf("%d,%d", row, col-1), fmt.Sprintf("%d,%d", row, col+1)}
+						newNodeKeys = append(newNodeKeys, col-1, col+1)
+						nodeKey = fmt.Sprintf("%d,%d", row-1, col)
 					}
 				}
 
+				nodes = newNodeKeys
 			}
-
 		}
 	}
 
 	count := 0
 
-	for _, path := range paths {
-		if len(path.concatenatedString) == fileLength {
-			count++
-		}
-	}
+	dfs(tree, "1,7", make(map[string]bool), &count)
+
+	// for _, path := range paths {
+	// 	if len(path.concatenatedString) == fileLength {
+	// 		count++
+	// 	}
+	// }
 
 	fmt.Println(count)
+}
+
+func dfs(tree map[string][]string, start string, visited map[string]bool, count *int) {
+
+	if visited[start] {
+		return
+	}
+	visited[start] = true
+
+	if strings.Contains(start, "15,") {
+		*count++
+	}
+
+	for _, neighbor := range tree[start] {
+		dfs(tree, neighbor, visited, count)
+	}
 }
 
 func checkCollictionOrTraverseVertically(input []string, i int, indexX int, paths []PathIndex, parentIndex int) []PathIndex {
