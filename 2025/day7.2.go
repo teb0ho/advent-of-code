30b9856e12192deebffd86a0e28a82537eb0ebc9
Teboho Mphure <tebohomphure@gmail.com>
2026-09-04 23:55:00 +0200

    Refactor path handling in laboratoriesPart2 to streamline tree construction and remove unused code

--- 2025/day7.2.go
+++ 2025/day7.2.go
@@ -3,15 +3,8 @@ package main
 import (
 	"fmt"
 	"regexp"
-	"slices"
 	"strings"
 )
-
-type PathIndex struct {
-	concatenatedString string
-	indexX             int
-	indexY             int
-}
 
 func laboratoriesPart2(input []string) {
 	sPosition := strings.Index(input[0], "S")
@@ -39,8 +32,7 @@ func laboratoriesPart2(input []string) {
 	}
 
 	var tree map[string][]string = make(map[string][]string)
-	// traverse all paths and check if they are valid
-	// LLDDRR
+
 	nodes := []int{}
 	nodeKey := ""
 	for row := 1; row < len(input); row++ {
@@ -55,7 +47,10 @@ func laboratoriesPart2(input []string) {
 			if len(nodes) != 0 {
 				newNodeKeys := []int{}
 				for _, col := range nodes {
-					if string(input[row][col]) == "|" {
+					if string(input[row][col]) == "|" && len(input)-1 == row {
+						tree[nodeKey] = []string{}
+						continue
+					} else if string(input[row][col]) == "|" {
 						tree[nodeKey] = []string{fmt.Sprintf("%d,%d", row, col)}
 						nodeKey = fmt.Sprintf("%d,%d", row, col)
 						newNodeKeys = append(newNodeKeys, col)
@@ -76,12 +71,6 @@ func laboratoriesPart2(input []string) {
 
 	dfs(tree, "1,7", make(map[string]bool), &count)
 
-	// for _, path := range paths {
-	// 	if len(path.concatenatedString) == fileLength {
-	// 		count++
-	// 	}
-	// }
-
 	fmt.Println(count)
 }
 
@@ -99,39 +88,4 @@ func dfs(tree map[string][]string, start string, visited map[string]bool, count 
 	for _, neighbor := range tree[start] {
 		dfs(tree, neighbor, visited, count)
 	}
-}
-
-func checkCollictionOrTraverseVertically(input []string, i int, indexX int, paths []PathIndex, parentIndex int) []PathIndex {
-	indexOfReferenceElement := findElementIndex(paths, indexX, parentIndex)
-
-	if indexOfReferenceElement == -1 {
-		return paths
-	}
-
-	if string(input[i][indexX-1]) == "|" {
-		paths = append(paths, PathIndex{concatenatedString: paths[indexOfReferenceElement].concatenatedString + string(input[i][indexX-1]), indexX: indexX - 1, indexY: i})
-	}
-
-	if string(input[i][indexX+1]) == "|" {
-		paths = append(paths, PathIndex{concatenatedString: paths[indexOfReferenceElement].concatenatedString + string(input[i][indexX+1]), indexX: indexX + 1, indexY: i})
-	}
-
-	// remove the parent path from the paths slice
-
-	paths = slices.Delete(paths, indexOfReferenceElement, indexOfReferenceElement+1)
-
-	return paths
-}
-
-func findElementIndex(paths []PathIndex, indexX int, parentIndex int) int {
-	indexOfReferenceElement := -1
-
-	for i, path := range paths {
-		if path.indexX == indexX && path.indexY == parentIndex {
-			indexOfReferenceElement = i
-			break
-		}
-	}
-
-	return indexOfReferenceElement
 }
